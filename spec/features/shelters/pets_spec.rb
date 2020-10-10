900715cc5c9b6e1ca424af18a5abfb469f5c98c7
Kate Tester <katemorris@gmail.com>
2020-10-10 15:56:50 -0600

    Adds consistent links to pet show page.

--- spec/features/shelters/pets_spec.rb
+++ spec/features/shelters/pets_spec.rb
@@ -22,10 +22,12 @@ describe "As a visitor" do
       visit "/shelters/#{shelter_1.id}/pets"
 
       expect(page).to have_content(pet_1.name)
+      expect(page).to have_link(pet_1.name)
       expect(page).to have_xpath("//img[contains(@src,'#{pet_1.image}')]")
       expect(page).to have_content(pet_1.approximate_age)
       expect(page).to have_content(pet_1.sex)
       expect(page).to have_content(pet_2.name)
+      expect(page).to have_link(pet_2.name)
       expect(page).to have_xpath("//img[contains(@src,'#{pet_2.image}')]")
       expect(page).to have_content(pet_2.approximate_age)
       expect(page).to have_content(pet_2.sex)

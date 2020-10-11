c52a48952ecdaf24eb76769d6b39e1481d27bf04
Kate Tester <katemorris@gmail.com>
2020-10-10 19:28:56 -0600

    Adds tests for adoptable/pending filter methods.

--- spec/models/pets_spec.rb
+++ spec/models/pets_spec.rb
@@ -68,5 +68,73 @@ describe Pet, type: :model do
       expect(shelter.pets.adoptable_first).to eq([pet_1, pet_3, pet_2])
       expect(shelter.pets.adoptable_first).to_not eq([pet_1, pet_2, pet_3])
     end
+
+    it '.adoptable_filter' do
+      shelter = Shelter.create(
+        name: 'Austin Pets Alive!',
+        address: '123 Happy Ln',
+        city: 'Austin',
+        state: 'TX',
+        zip: '78704'
+      )
+      pet_1 = Pet.create(
+        name: 'Arwen',
+        approximate_age: 2,
+        sex: 'female',
+        status: 'Adoptable',
+        shelter_id: shelter.id
+      )
+      pet_2 = Pet.create(
+        name: 'Nessa',
+        approximate_age: 1,
+        sex: 'female',
+        status: 'Pending',
+        shelter_id: shelter.id
+      )
+      pet_3 = Pet.create(
+        name: 'Longhair',
+        approximate_age: 1,
+        sex: 'male',
+        status: 'Adoptable',
+        shelter_id: shelter.id
+      )
+
+      expect(shelter.pets.adoptable_filter).to eq([pet_1, pet_3])
+      expect(shelter.pets.adoptable_first).to_not eq([pet_1, pet_2, pet_3])
+    end
+
+    it '.pending_filter' do
+      shelter = Shelter.create(
+        name: 'Austin Pets Alive!',
+        address: '123 Happy Ln',
+        city: 'Austin',
+        state: 'TX',
+        zip: '78704'
+      )
+      pet_1 = Pet.create(
+        name: 'Arwen',
+        approximate_age: 2,
+        sex: 'female',
+        status: 'Adoptable',
+        shelter_id: shelter.id
+      )
+      pet_2 = Pet.create(
+        name: 'Nessa',
+        approximate_age: 1,
+        sex: 'female',
+        status: 'Pending',
+        shelter_id: shelter.id
+      )
+      pet_3 = Pet.create(
+        name: 'Longhair',
+        approximate_age: 1,
+        sex: 'male',
+        status: 'Adoptable',
+        shelter_id: shelter.id
+      )
+
+      expect(shelter.pets.adoptable_filter).to eq([pet_2])
+      expect(shelter.pets.adoptable_first).to_not eq([pet_1, pet_2, pet_3])
+    end
   end
 end
